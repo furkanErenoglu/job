4080176564d75de9eb61a2ae481061ef8bb562ec
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-05-16 22:50:18 +0300

    [DE-08]-created certificate class

---
+++ src/main/java/com/jobportal/job/model/Certificate.java
@@ -0,0 +1,37 @@
+package com.jobportal.job.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "certificates")
+public class Certificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "certificate_hours")
+    private int CertificateHours;
+}
