50467a4d52b56dbc5827081e9f89445242041c28
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-05-24 14:39:31 +0300

    [DE-11] Created Newtork Repo

---
+++ src/main/java/com/jobportal/job/repository/NetworkRepository.java
@@ -0,0 +1,7 @@
+package com.jobportal.job.repository;
+
+import com.jobportal.job.model.Network;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NetworkRepository extends JpaRepository<Network, Long> {
+}
