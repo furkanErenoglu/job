c007ea30c6f380fa953deea4951a14048b3c1331
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-05-28 23:47:10 +0300

    [DE-13]- Added new repositories methods

--- src/main/java/com/jobportal/job/repository/JobRepository.java
+++ src/main/java/com/jobportal/job/repository/JobRepository.java
@@ -4,4 +4,5 @@ import com.jobportal.job.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+    Job findByUuid(String uuid);
 }
