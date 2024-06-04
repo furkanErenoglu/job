6b724017624496a2784bdc49064917a4e78e8b01
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-04 13:38:17 +0300

    [DE-16]- Created Certificate Repository

--- src/main/java/com/jobportal/job/repository/EducationRepository.java
+++ src/main/java/com/jobportal/job/repository/EducationRepository.java
@@ -3,5 +3,5 @@ package com.jobportal.job.repository;
 import com.jobportal.job.model.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EducationRepository extends JpaRepository<Education, Long> {
+public interface EducationRepository extends JpaRepository< Education, Long> {
 }
