8b556b8ab0748485dab579b343a5305ddda89de2
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-13 13:59:12 +0300

    [DE-18] created Post Update

---
+++ src/main/java/com/jobportal/job/model/Post.java
@@ -0,0 +1,36 @@
+package com.jobportal.job.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "user_uuids")
+    private String likedUsers;
+    @Column(name = "created_date")
+    private Timestamp createdDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private com.archisacademy.jobportal.model.User user;
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+}
