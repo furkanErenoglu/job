dedaff1ed7eb5d924578b55e8ba4c5f5950efdab
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-20 23:16:04 +0300

    [DE-18] updated Experience

--- src/main/java/com/jobportal/job/model/Experience.java
+++ src/main/java/com/jobportal/job/model/Experience.java
@@ -1,10 +1,12 @@
 package com.jobportal.job.model;
 
+import com.jobportal.job.enums.LocationType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 
 @Data
@@ -14,31 +16,27 @@ import java.time.LocalDate;
 @Table(name = "experiences")
 public class Experience {
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
-    @Column(name = "title")
-    private String title;
-
+    @Column(name = "company_name")
+    private String companyName;
+    @Column(name = "is_active")
+    private boolean isActive;
+    @Column(name = "start_date")
+    private Timestamp startDate;
+    @Column(name = "end_date")
+    private Timestamp endDate;
     @Column(name = "location")
     private String location;
-
+    @Enumerated(EnumType.STRING)
     @Column(name = "location_type")
-    private String locationType;
-
-    @Column(name = "job_type")
-    private String jobType;
-
-    @Column(name = "company")
-    private String company;
-
-    @Column(name = "starting_date")
-    private LocalDate startingDate;
-
-    @Column(name = "end_date")
-    private LocalDate endDate;
-
+    private LocationType locationType;
+    @Column(name = "position")
+    private String position;
     @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
 }
