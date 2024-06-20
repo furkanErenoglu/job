cb153af574f4e575e970fa079451e19d99f14234
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-20 23:18:29 +0300

    [DE-18] created Connection class

---
+++ src/main/java/com/jobportal/job/model/Connection.java
@@ -0,0 +1,34 @@
+package com.jobportal.job.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name = "connections")
+public class Connection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private ConnectionStatus status;
+    @Column(name = "request_date")
+    private Timestamp requestDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private com.archisacademy.jobportal.model.User user;
+    @ManyToOne
+    @JoinColumn(name = "requested_user_id")
+    private com.archisacademy.jobportal.model.User requestedUser;
+
+}
