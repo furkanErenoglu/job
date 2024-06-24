2aa7c4c766b04a68c0026a88cd2ebf6d66e69b23
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-24 23:28:48 +0300

    [DE-19] Created UserPostCommentMapper Repository

---
+++ src/main/java/com/jobportal/job/repository/UserPostCommentMapperRepository.java
@@ -0,0 +1,7 @@
+package com.jobportal.job.repository;
+
+import com.jobportal.job.model.UserPostCommentMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPostCommentMapperRepository extends JpaRepository<UserPostCommentMapper, Long> {
+}
