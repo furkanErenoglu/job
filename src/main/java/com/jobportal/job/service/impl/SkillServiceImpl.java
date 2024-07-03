c98d6dbe1f4ecbb8a7466ef004e693836d0146b5
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-07-03 19:20:48 +0300

    [DE-20]- Update SkillService

--- src/main/java/com/jobportal/job/service/impl/SkillServiceImpl.java
+++ src/main/java/com/jobportal/job/service/impl/SkillServiceImpl.java
@@ -2,27 +2,48 @@ package com.jobportal.job.service.impl;
 
 import com.jobportal.job.dtos.SkillDto;
 import com.jobportal.job.model.Skills;
+import com.jobportal.job.repository.ProfileRepository;
+import com.jobportal.job.repository.SkillsRepository;
 import com.jobportal.job.service.SkillService;
+import jakarta.transaction.Transactional;
 
 public class SkillServiceImpl implements SkillService {
+    private final SkillsRepository skillsRepository;
+    private final ProfileRepository profileRepository;
+
+    public SkillServiceImpl(SkillsRepository skillsRepository, ProfileRepository profileRepository) {
+        this.skillsRepository = skillsRepository;
+        this.profileRepository = profileRepository;
+    }
+
     @Override
+    @Transactional
     public String createSkill(SkillDto skillDto) {
-        return null;
+        Skills skills = convertToEntity(skillDto);
+        skills.setProfile(profileRepository.findById(Long.parseLong(skillDto.getProfileId())).orElseThrow());
+        skillsRepository.save(skills);
+        return "Skill created successfully";
     }
 
     @Override
+    @Transactional
     public String updateSkill(SkillDto skillDto) {
-        return null;
+        Skills existingSkill = skillsRepository.findById(Long.parseLong(skillDto.getProfileId())).get();
+        existingSkill.setName(skillDto.getName());
+        existingSkill.setDescription(skillDto.getDescription());
+        return "Skill updated successfully";
     }
 
     @Override
+    @Transactional
     public String deleteSkill(long skillId) {
-        return null;
+        skillsRepository.deleteById(skillId);
+        return "Skill deleted successfully";
     }
 
     @Override
     public SkillDto getSkill(long skillId) {
-        return null;
+        return convertToDto(skillsRepository.findById(skillId).get());
     }
 
     private SkillDto convertToDto(Skills skills) {
