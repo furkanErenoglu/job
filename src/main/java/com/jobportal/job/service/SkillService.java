758e9c925953dcae18185eb27bf0148f7e218360
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-07-03 17:36:00 +0300

    [DE-20]- Created Skill Service Interface

--- src/main/java/com/jobportal/job/service/SkillService.java
+++ src/main/java/com/jobportal/job/service/SkillService.java
@@ -1,5 +1,10 @@
 package com.jobportal.job.service;
 
+import com.jobportal.job.dtos.SkillDto;
+
 public interface SkillService {
-    String createSkill();
+    String createSkill(SkillDto skillDto);
+    String updateSkill(SkillDto skillDto);
+    String deleteSkill(long skillId);
+    SkillDto getSkill(long skillId);
 }
