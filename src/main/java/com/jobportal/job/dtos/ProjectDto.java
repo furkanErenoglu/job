f1194ee0ec5e55e3dce4523bea1682be4cc57679
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-07-13 22:36:35 +0300

    [DE-23]- Created ProjectDto

---
+++ src/main/java/com/jobportal/job/dtos/ProjectDto.java
@@ -0,0 +1,25 @@
+package com.jobportal.job.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectDto {
+    private String projectName;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private String position;
+    private Boolean status;
+    private String url;
+    private List<String> technologies;
+    private String description;
+    private Long profileId;
+}
