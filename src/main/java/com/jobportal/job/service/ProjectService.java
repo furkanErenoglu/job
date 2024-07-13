55ad9ca131f614eb1971f439089aafc217162574
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-07-13 22:38:32 +0300

    [DE-23]- Created Project Service Interface

---
+++ src/main/java/com/jobportal/job/service/ProjectService.java
@@ -0,0 +1,15 @@
+package com.jobportal.job.service;
+
+import com.jobportal.job.dtos.ProjectDto;
+import com.jobportal.job.model.Project;
+
+import java.util.List;
+
+public interface ProjectService {
+    String createProject(ProjectDto project);
+    String deleteProject(Long id);
+    String updateProject(ProjectDto project);
+    List<ProjectDto> getAllProjects();
+    ProjectDto getProjectById(Long id);
+    Project getProjectEntityById(Long id);
+}
