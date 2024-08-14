18cbb645c6748b2bcb12d86c95b488a1363a24ba
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-08-14 21:55:45 +0300

    [DE-30]- Created Certificate Dto

---
+++ src/main/java/com/jobportal/job/dtos/CertificateDto.java
@@ -0,0 +1,22 @@
+package com.jobportal.job.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CertificateDto {
+    private String name;
+    private String companyName;
+    private Timestamp postingDate;
+    private int hours;
+    private String url;
+    private long profileId;
+
+}
