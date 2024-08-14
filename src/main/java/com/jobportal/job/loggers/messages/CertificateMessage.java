26a7ee2b7e03a8fcc794c326d142035a390b36a6
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-08-14 21:56:46 +0300

    [DE-30]- Created Certificate Message

---
+++ src/main/java/com/jobportal/job/loggers/messages/CertificateMessage.java
@@ -0,0 +1,12 @@
+package com.jobportal.job.loggers.messages;
+
+public class CertificateMessage {
+    private CertificateMessage(){
+        throw new UnsupportedOperationException("Utility class can not be instantiated");
+    }
+
+    public static final String CERTIFICATE_CREATED = "Certificate has been created successfully with id: ";
+    public static final String CERTIFICATE_DELETED = "Certificate has been deleted successfully with id: ";
+    public static final String CERTIFICATE_UPDATED = "Certificate has been updated successfully with id: ";
+    public static final String CERTIFICATE_NOT_FOUND = "Certificate not found with id: ";
+}
